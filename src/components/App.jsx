ac97b636be3925c20b00e01648f30b8fb995eb05
Luciiifr <aimconge.bln@gmail.com>
2024-04-20 19:48:24 +0800

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,14 @@
-import {Profile} from './Profile/Profile'
+import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -11,6 +20,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload Stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
